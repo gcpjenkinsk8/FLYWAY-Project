1aeaad83b062a7ec9efd2170220ac8cc78a2e50c
pmgitrep <133342915+pmgitrep@users.noreply.github.com>
2023-07-24 11:49:33 +0530

    Update V4__alter_tb.sql

--- migrations/V4__alter_tb.sql
+++ migrations/V4__alter_tb.sql
@@ -1,8 +1,2 @@
 -- create a table Companies with name, id, address, email, and phone number
-ALTER TABLE CompaniesNew rename Companiesold (
-  id int,
-  name varchar(50),
-  fax text,
-  emailid varchar(50),
-  phone varchar(10)
-);
+ALTER TABLE CompaniesNew rename Companiesold;
