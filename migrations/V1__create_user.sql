a6131cbb9e161fe37f0749743fffb59d7f463b3f
pmgitrep <133342915+pmgitrep@users.noreply.github.com>
2023-07-24 16:36:10 +0530

    Update V1__create_user.sql

--- migrations/V1__create_user.sql
+++ migrations/V1__create_user.sql
@@ -1,5 +1,5 @@
 -- create a table Companies with name, id, address, email, and phone number
-CREATE TABLE CompaniesNew (
+CREATE TABLE CompaniesNew1 (
   id int,
   name varchar(50),
   address text,
