afeeb8283a03a0a0e2ec7f4519913021d798c87c
pmgitrep <133342915+pmgitrep@users.noreply.github.com>
2023-07-24 11:57:51 +0530

    Delete V2__create_emp.sql

--- migrations/V2__create_emp.sql
+++
@@ -1,8 +0,0 @@
--- create a table Companies with name, id, address, email, and phone number
-CREATE TABLE Employee (
-  id int,
-  name varchar(50),
-  address text,
-  email varchar(50),
-  phone varchar(10)
-);
