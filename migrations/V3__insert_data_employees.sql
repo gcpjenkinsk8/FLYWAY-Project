8c2a9e5d32c25149218f2a5acd441f29c35ee9d5
pmgitrep <133342915+pmgitrep@users.noreply.github.com>
2023-07-26 02:47:18 +0530

    Create V3__insert_data_employees.sql

---
+++ migrations/V3__insert_data_employees.sql
@@ -0,0 +1,7 @@
+INSERT INTO employees (employee_id, first_name, last_name, department, salary)
+VALUES 
+  (1, 'John', 'Doe', 'IT', 50000),
+  (2, 'Jane', 'Smith', 'HR', 45000),
+  (3, 'Michael', 'Johnson', 'Finance', 60000),
+  (4, 'Emily', 'Brown', 'Marketing', 55000),
+  (5, 'David', 'Lee', 'Operations', 52000);
