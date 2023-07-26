9952c3ba0feb0ca31b70edc73516911fd215e826
pmgitrep <133342915+pmgitrep@users.noreply.github.com>
2023-07-26 14:56:00 +0530

    Delete V3__insert_data_employees.sql

--- migrations/V3__insert_data_employees.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO employees12 (employee_id, first_name, last_name, department, salary)
-VALUES 
-  (1, 'John', 'Doe', 'IT', 50000),
-  (2, 'Jane', 'Smith', 'HR', 45000),
-  (3, 'Michael', 'Johnson', 'Finance', 60000),
-  (4, 'Emily', 'Brown', 'Marketing', 55000),
-  (5, 'David', 'Lee', 'Operations', 52000);
