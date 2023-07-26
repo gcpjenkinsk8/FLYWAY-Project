91df5fa88031ad33cc810f4e7cafa73e9fdff2e9
pmgitrep <133342915+pmgitrep@users.noreply.github.com>
2023-07-26 14:56:30 +0530

    Update V1__create_table_employees12.sql

--- migrations/V1__create_table_employees12.sql
+++ migrations/V1__create_table_employees12.sql
@@ -1,7 +1,7 @@
-CREATE TABLE employees12 (
-  employee_id INT PRIMARY KEY,
-  first_name VARCHAR(50),
-  last_name VARCHAR(50),
-  department VARCHAR(100),
-  salary DECIMAL(10, 2)
+CREATE TABLE employee (
+  id int,
+  name varchar(50),
+  address text,
+  email varchar(50),
+  phone varchar(10)
 );
