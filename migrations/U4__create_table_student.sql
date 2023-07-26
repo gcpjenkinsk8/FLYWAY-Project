9521910392cee38ffb627d934b0e9b2dc63eecaa
pmgitrep <133342915+pmgitrep@users.noreply.github.com>
2023-07-26 22:37:10 +0530

    Update U4__create_table_student.sql

--- migrations/U4__create_table_student.sql
+++ migrations/U4__create_table_student.sql
@@ -1,8 +1,2 @@
 -- create a table Companies with name, id, address, email, and phone number
-DROP TABLE Student (
-  id int,
-  name varchar(50),
-  address text,
-  email varchar(50),
-  phone varchar(10)
-);
+DROP TABLE Student;
