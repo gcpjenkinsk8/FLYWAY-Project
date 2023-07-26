6a0eb3bac3312fa1634365d88207519f0b0c1b74
pmgitrep <133342915+pmgitrep@users.noreply.github.com>
2023-07-26 16:30:53 +0530

    Create V3__create_table_abc.sql

---
+++ migrations/V3__create_table_abc.sql
@@ -0,0 +1,9 @@
+-- create a table Companies with name, id, address, email, and phone number
+dsdCREATE TABLE Companies (
+  id int,
+  name varchar(50),
+  address text,
+  email varchar(50),
+  phone varchar(10)
+);
+fdf
