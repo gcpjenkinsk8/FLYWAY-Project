a26c8496910c60bbdd0434cbacf4d64de8e2c4b8
pmgitrep <133342915+pmgitrep@users.noreply.github.com>
2023-07-31 15:14:22 +0530

    Create V5__create_insert_table_v5.sql

---
+++ migrations/V5__create_insert_table_v5.sql
@@ -0,0 +1,12 @@
+CREATE TABLE v5 (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    email VARCHAR(100)
+);
+
+INSERT INTO services (id, name, age, email)
+VALUES
+    (1, 'John Doe', 30, 'john@example.com'),
+    (2, 'Jane Smith', 25, 'jane@example.com'),
+    (3, 'Bob Johnson', 28, 'bob@example.com');
