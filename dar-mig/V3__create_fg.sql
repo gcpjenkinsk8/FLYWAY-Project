40910b6322333057ac0f30809487e241df44a018
pmgitrep <133342915+pmgitrep@users.noreply.github.com>
2023-08-04 14:40:49 +0530

    newfig

--- dar-mig/V3__create_fg.sql
+++ dar-mig/V3__create_fg.sql
@@ -1,15 +1,15 @@
-CREATE PIPELINE `figRewards`
+CREATE PIPELINE figRewards
 AS LOAD DATA LINK link_kafka_dev 'dev-0-digitalassetresearch-b9b3.aivencloud.com:23893/figment_rewards'
 BATCH_INTERVAL 1000
 MAX_PARTITIONS_PER_BATCH 1
 ENABLE OUT_OF_ORDER OPTIMIZATION
 DISABLE OFFSETS METADATA GC
 REPLACE
-INTO TABLE `figEthRewards`
+INTO TABLE figEthRewards
 FORMAT JSON
 (
-    `figEthRewards`.`dateReward` <- `date`,
-    `figEthRewards`.`value` <- `value`,
-    `figEthRewards`.`staking` <- `staking`,
-    `figEthRewards`.`tips` <- `tips`
+    figEthRewards.dateReward <- date,
+    figEthRewards.value <- value,
+    figEthRewards.staking <- staking,
+    figEthRewards.tips <- tips
 )
