004484c69aae9ddaf9624781d2da911e5b37356d
pmgitrep <133342915+pmgitrep@users.noreply.github.com>
2023-08-04 14:38:20 +0530

    figre

--- dar-mig/V3__create_fg.sql
+++ dar-mig/V3__create_fg.sql
@@ -1,5 +1,5 @@
 CREATE PIPELINE `figRewards`
-AS LOAD DATA LINK link_kafka_prod 'dev-0-digitalassetresearch-b9b3.aivencloud.com:23893/figment_rewards'
+AS LOAD DATA LINK link_kafka_dev 'dev-0-digitalassetresearch-b9b3.aivencloud.com:23893/figment_rewards'
 BATCH_INTERVAL 1000
 MAX_PARTITIONS_PER_BATCH 1
 ENABLE OUT_OF_ORDER OPTIMIZATION
