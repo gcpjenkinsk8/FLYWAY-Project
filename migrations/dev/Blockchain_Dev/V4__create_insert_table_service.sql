1473dd13ddf8511806da03ea92c3515b169fca01
pmgitrep <133342915+pmgitrep@users.noreply.github.com>
2023-08-09 02:22:17 +0530

    ..

--- migrations/dev/Blockchain_Dev/V4__create_insert_table_service.sql
+++ migrations/dev/Blockchain_Dev/V4__create_insert_table_service.sql
@@ -1,15 +1,7 @@
-CREATE PIPELINE `figRewards`
-AS LOAD DATA LINK link_kafka_dev 'nonprod-0-digitalassetresearch-b9b3.aivencloud.com:23893/figment_rewards_test'
-BATCH_INTERVAL 1000
-MAX_PARTITIONS_PER_BATCH 1
-ENABLE OUT_OF_ORDER OPTIMIZATION
-DISABLE OFFSETS METADATA GC
-REPLACE
-INTO TABLE `figEthRewards`
-FORMAT JSON
-(
-    `figEthRewards`.`dateReward` <- `date`,
-    `figEthRewards`.`value` <- `value`,
-    `figEthRewards`.`staking` <- `staking`,
-    `figEthRewards`.`tips` <- `tips`
-)
+CREATE TABLE CompaniesZ (
+  id int,
+  name varchar(50),
+  address text,
+  email varchar(50),
+  phone varchar(10)
+);
